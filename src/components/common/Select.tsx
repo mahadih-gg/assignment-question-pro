e2a8de8cc6e5ea921411f42c642ef6642e54744c
mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2024-04-23 16:42:24 +0600

    my component feature added

--- src/components/common/Select.tsx
+++ src/components/common/Select.tsx
@@ -8,21 +8,26 @@ interface PropsType {
 }
 
 const Select: React.FC<PropsType> = ({ data, setId, valueKey, optionKey }) => {
+
   return (
-    <select onChange={(e) => setId(e.target.value)} className='w-56 outline-none border border-secondary p-2 rounded-md truncate ...'>
-      {
-        data?.map((item, i: number) => {
-          return (
-            <option
-              key={i}
-              value={item[valueKey]}
-            >
-              {item[optionKey]}
-            </option>
-          )
-        })
-      }
-    </select>
+    <>
+      <input list="brow" />
+      <select onChange={(e) => setId(e.target.value)} className='w-56 outline-none border border-secondary p-2 rounded-md truncate ...'>
+        <option value="">Select...</option>
+        {
+          data?.map((item, i: number) => {
+            return (
+              <option
+                key={i}
+                value={item[valueKey]}
+              >
+                {item[optionKey]}
+              </option>
+            )
+          })
+        }
+      </select>
+    </>
   );
 };
 
--- src/pages/MyComponentPage.tsx
+++ src/pages/MyComponentPage.tsx
@@ -1,15 +1,46 @@
+import { useEffect, useState } from 'react';
 import Button from '../components/common/Button';
 
 const MyComponentPage = () => {
+
+  const [count, setCount] = useState<number>(0)
+
+  useEffect(() => {
+    const firstButton = document.querySelector('button');
+    if (firstButton) {
+      firstButton.addEventListener('click', handleClick);
+    }
+
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button, index) => {
+      if (index !== 0) {
+        button.classList.add('btn-disabled');
+      }
+      button.addEventListener('click', handleClick);
+    });
+
+    return () => {
+      if (firstButton) {
+        firstButton.removeEventListener('click', handleClick);
+      }
+    };
+  }, []);
+
+  const handleClick = () => {
+    setCount((prevState) => prevState + 1)
+  };
+
   return (
     <div className='grid gap-5 py-section'>
-      This is My component...
-      <Button>
-        Button 1
-      </Button>
-      <Button type='disabled'>
-        Button 2
-      </Button>
+      <p className='flex-center gap-5'>Button Click: <span className='text-xl text-primary'>{count}</span></p>
+
+      {
+        [1, 2, 3, 4, 5].map((item: number, i: number) => {
+          return (
+            <Button key={i}><span>Button {item}</span></Button>
+          )
+        })
+      }
     </div>
   );
 };
